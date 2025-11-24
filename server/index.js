9e500813185acc3be71a239d342b9405e7b72bc2
“[Vidura <Vidura1159@gmail.com>
2025-11-24 16:37:04 +0530

    Prepare for deployment: Update port, CORS, seed script, and API client

--- server/index.js
+++ server/index.js
@@ -49,57 +49,6 @@ const authController = new AuthController(authService);
 const productController = new ProductController(productService);
 const orderController = new OrderController(orderService);
 
-const fastify = require('fastify')({ logger: true });
-const cors = require('@fastify/cors');
-const path = require('path');
-
-// Infrastructure
-const dbConnection = require('./src/infrastructure/database/DatabaseConnection');
-const SQLiteUserRepository = require('./src/infrastructure/repositories/SQLiteUserRepository');
-const SQLiteProductRepository = require('./src/infrastructure/repositories/SQLiteProductRepository');
-const SQLiteOrderRepository = require('./src/infrastructure/repositories/SQLiteOrderRepository');
-
-// Application
-const AuthService = require('./src/application/services/AuthService');
-const ProductService = require('./src/application/services/ProductService');
-const OrderService = require('./src/application/services/OrderService');
-
-// Strategy Pattern - Payment Strategies
-const CashPaymentStrategy = require('./src/application/strategies/CashPaymentStrategy');
-const CreditCardPaymentStrategy = require('./src/application/strategies/CreditCardPaymentStrategy');
-
-// Observer Pattern - Observers
-const InventoryObserver = require('./src/application/observers/InventoryObserver');
-const NotificationObserver = require('./src/application/observers/NotificationObserver');
-
-// Interface
-const AuthController = require('./src/interfaces/http/controllers/AuthController');
-const ProductController = require('./src/interfaces/http/controllers/ProductController');
-const OrderController = require('./src/interfaces/http/controllers/OrderController');
-const routes = require('./src/interfaces/http/routes');
-
-// Dependency Injection Container (Manual)
-const userRepository = new SQLiteUserRepository();
-const productRepository = new SQLiteProductRepository();
-const orderRepository = new SQLiteOrderRepository();
-
-const authService = new AuthService(userRepository);
-const productService = new ProductService(productRepository);
-
-// Initialize OrderService with Cash payment strategy by default
-const cashPaymentStrategy = new CashPaymentStrategy();
-const orderService = new OrderService(orderRepository, productRepository, cashPaymentStrategy);
-
-// Subscribe observers to OrderService (Observer Pattern)
-const inventoryObserver = new InventoryObserver();
-const notificationObserver = new NotificationObserver();
-orderService.subscribe(inventoryObserver);
-orderService.subscribe(notificationObserver);
-
-const authController = new AuthController(authService);
-const productController = new ProductController(productService);
-const orderController = new OrderController(orderService);
-
 // Register CORS
 fastify.register(cors, {
   origin: true
